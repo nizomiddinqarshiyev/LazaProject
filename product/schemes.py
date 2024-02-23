16f4586dc6b8eba38986877ec7240c8abe198b8f
WARRIORORDEFENDER <124035330+WARRIORORDEFENDER@users.noreply.github.com>
2024-02-23 17:16:40 +0500

    Produt almost done

--- product/schemes.py
+++ product/schemes.py
@@ -3,7 +3,44 @@ from enum import Enum
 from pydantic import BaseModel
 
 
+
+class get_category(BaseModel):
+    id: int
+    name: str
+
+
+class subcategroy_list(BaseModel):
+    id: int
+    name: str
+    category_id: get_category
+
+class subcategroy_list_for_product(BaseModel):
+    id: int
+    name: str
+    category_id: int
+
+
+class Brands(BaseModel):
+    id: int
+    name: str
+
+
+
 class get_product_list(BaseModel):
+    id: int
+    brand_id: Brands
+    name: str
+    price: float
+    quantity: int
+    created_at: datetime
+    sold_quantity: int
+    description: str
+    category_id: get_category
+    subcategory_id: subcategroy_list_for_product
+
+
+
+class get_product_list_for_productcolor(BaseModel):
     id: int
     brand_id: int
     name: str
@@ -26,20 +63,37 @@ class add_product(BaseModel):
     subcategory_id: int
 
 
-class get_category(BaseModel):
-    id:int
-    name:str
+
+
 
 class add_category(BaseModel):
-    name :str
+    name: str
 
 
-class subcategroy_list(BaseModel):
-    id:int
-    name:str
-    category_id:int
+
 
 
 class Add_subcategory(BaseModel):
-    name:str
-    category_id:int
+    name: str
+    category_id: int
+
+
+class BrandsAdd(BaseModel):
+    name: str
+
+
+class Colors(BaseModel):
+    id: int
+    code: str
+
+
+class ColorsAdd(BaseModel):
+    code: str
+
+
+class ProductColors(BaseModel):
+    id: int
+    product_id: get_product_list_for_productcolor
+    color_id: Colors
+
+
